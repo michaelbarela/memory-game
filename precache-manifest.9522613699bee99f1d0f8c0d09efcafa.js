8676109600a1aead614534f5b14b6cea8276c6ab
Michael B <michaelbarela17@gmail.com>
2019-04-22 16:18:00 -0700

    Updates

---
+++ precache-manifest.9522613699bee99f1d0f8c0d09efcafa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "7f2b041c408d5a59718d",
+    "url": "/memory-game/static/js/main.8ae44360.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7f2b041c408d5a59718d",
+    "url": "/memory-game/static/css/main.82b27d50.chunk.css"
+  },
+  {
+    "revision": "8070323fcd2cfa82e97d371928a952f3",
+    "url": "/memory-game/index.html"
+  }
+];
